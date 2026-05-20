2364080a3b6e314c43569b05d6c348641ec03aff
tanvir321110 <tanvirahmed321110@gmail.com>
2026-05-20 12:55:09 +0600

    dashboard date search

--- controllers/dashboard.py
+++ controllers/dashboard.py
@@ -15,18 +15,36 @@ class DashboardController(BasePortalController,http.Controller):
             ('user_id', '=', user.id)
         ], limit=1)
         name = employee.name or ''
+        base_domain = [ ('create_uid', '=', user.id)]
 
-
-        requisitions = request.env['local.purchase.requisition'].sudo().search([
-            ('create_uid', '=', user.id)
+        # all requisitions
+        requisitions = request.env['local.purchase.requisition'].sudo().search(base_domain)
+        # Forwarded
+        forwarded_count = request.env['local.purchase.requisition'].sudo().search_count([
+            *base_domain,
+            '|',
+            '|',
+            ('state', '=', 'draft'),
+            ('state', '=', 'forwarded'),
+            ('state', '=', 'recommended')
         ])
-        products = request.env['product.product'].sudo().search([])
+        approved_count = request.env['local.purchase.requisition'].sudo().search_count([
+            *base_domain,
+            '|',
+            ('state', '=', 'supplied'),
+            ('state', '=', 'approved')
+        ])
+        cancelled_count = request.env['local.purchase.requisition'].sudo().search_count(base_domain + [('state', '=', 'cancelled')])
+        # products = request.env['product.product'].sudo().search([])
 
         values.update({
-            'products': products,
+            # 'products': products,
             'name': name,
             'today': date.today().strftime('%Y-%m-%d'),
             'requisitions':requisitions,
+            'forwarded_count':forwarded_count,
+            'approved_count': approved_count,
+            'cancelled_count':cancelled_count,
         })
 
         return request.render('purchase_requisition_tds.portal_requisition_dashboard', values)
